bbb9636df51d0e5256298f2618bf2d83c6b97d8a
Karan Agarwal <karanyobro@gmail.com>
2021-11-30 19:58:52 +0530

    Create Addition.py

---
+++ Addition.py
@@ -0,0 +1,9 @@
+Num1 = int(input('First Number: '))
+Num2 = int(input('Second Number: '))
+Sum = Num1 + Num2
+print (Sum)
+
+
+# Advance Python
+
+print((float(input('Enter first number: '))+float(input('Enter second number: '))))
