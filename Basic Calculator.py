0cc3be36a2fd1217ce824898dda36ce0a6976d62
Karan Agarwal <karanyobro@gmail.com>
2021-11-30 21:28:08 +0530

    Update Basic Calculator.py

--- Basic Calculator.py
+++ Basic Calculator.py
@@ -1,11 +1,21 @@
 # Basic Python
 
-Num1 = input()
-Num2 = input()
+Num1 = int(input('First Number: '))
+Num2 = int(input('Second Number: '))
 
-sum = Num1 + Num2
+Sum = Num1 + Num2
 sub = Num1 - Num2
 div = Num1 / Num2
 into = Num1 * Num2
 
-print (sum, sub, div, into)
+print ('Addition is ', Sum)
+print ('Subtration is ', sub)
+print ('division is ', div)
+print ('multiplication is ', into)
+
+
+# Advance Python
+
+num1 = (int(input('First Number: ')))
+num2 = (int(input('Second Number: ')))
+print(num1+num2, num1-num2, num1/num2, num1*num2)
