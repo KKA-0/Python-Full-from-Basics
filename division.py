bd0044451396e3f82be0a81a549986656093a5c6
Karan Agarwal <karanyobro@gmail.com>
2021-11-30 20:11:04 +0530

    Create division.py

---
+++ division.py
@@ -0,0 +1,11 @@
+# Basic Python
+
+Num1 = int(input('First Number: '))
+Num2 = int(input('Second Number: '))
+div = Num1 / Num2
+print (div)
+
+
+# Advance Python
+
+#print((float(input('Enter first number: '))/float(input('Enter second number: '))))
