4cd99ee55516fceca967f8e19578485d08857f76
Karan Agarwal <karanyobro@gmail.com>
2021-11-30 22:16:27 +0530

    Create if-else.py

---
+++ if-else.py
@@ -0,0 +1,8 @@
+# Q.) If input is higher then 18 print you are an Adult, else print you're not Adult?
+
+A = int(input("type you're Age: "))
+if (A>=18):
+    print ("You are an Adult")
+else:
+    print("You are not an Adult")
+ 
