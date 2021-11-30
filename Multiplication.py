1b34a28f447475c238600d35854287b225563d8b
Karan Agarwal <karanyobro@gmail.com>
2021-11-30 20:14:25 +0530

    Update Multiplication.py

---
+++ Multiplication.py
@@ -0,0 +1,11 @@
+# Basic Python
+
+Num1 = int(input('First Number: '))
+Num2 = int(input('Second Number: '))
+into = Num1 * Num2
+print (into)
+
+
+# Advance Python
+
+#print((float(input('Enter first number: '))*float(input('Enter second number: '))))
