ab02e241a9cff921cd6f1f15d2247b7fcc5f6198
Karan Agarwal <karanyobro@gmail.com>
2021-11-30 20:07:06 +0530

    Create Subtraction.py

---
+++ Subtraction.py
@@ -0,0 +1,11 @@
+# Basic Python
+
+Num1 = int(input('First Number: '))
+Num2 = int(input('Second Number: '))
+sub = Num1 - Num2
+print (sub)
+
+
+# Advance Python
+
+#print((float(input('Enter first number: '))+float(input('Enter second number: '))))
